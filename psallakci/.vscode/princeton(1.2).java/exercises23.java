e5c54975f2db4b4fa9bc2b501e29c35308f78ca7
pelinsallakci <112391123+pelinsallakci@users.noreply.github.com>
2022-09-02 15:04:05 +0300

    builtintypes

---
+++ psallakci/.vscode/princeton(1.2).java/exercises23.java
@@ -0,0 +1,10 @@
+public class exercises23 {
+    public static void main(String[] args) {
+        int m = Integer.parseInt(args[0]);
+        int d = Integer.parseInt(args[1]);
+
+        boolean spring = (m == 3 && d >= 20 && d <= 31) || (m == 4 && d >= 1 && d <= 30)
+                || (m == 5 && d >= 1 && d <= 31) || (m == 6 && d >= 1 && d <= 20);
+        System.out.println(spring);
+    }
+}
