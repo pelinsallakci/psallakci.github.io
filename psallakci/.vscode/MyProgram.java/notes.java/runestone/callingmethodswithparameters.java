8c55d049687aa9bee74abfda43eaa23d86610814
pelinsallakci <112391123+pelinsallakci@users.noreply.github.com>
2022-09-09 12:33:31 +0300

    callingmethodswithparameters

---
+++ psallakci/.vscode/MyProgram.java/notes.java/runestone/callingmethodswithparameters.java
@@ -0,0 +1,12 @@
+public class callingmethodswithparameters {
+    public static void main(String[] args) {
+        // arguments/ actual parameters: the values that are given to methods to help
+        // them do their job.
+        // () after method names are used to hold data (actual parameter)
+        // formal parameters: the variables that are defined.
+        // overloaded: when there are multiple methods with the same name, different
+        // method signature
+        // object.method(); or object.method(arguments)
+
+    }
+}
