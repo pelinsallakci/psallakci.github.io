ae4e48d32c3d457adc59e7a5da047060d8f16814
pelinsallakci <112391123+pelinsallakci@users.noreply.github.com>
2022-09-13 11:17:29 +0300

    callingmethodsthatreturnvalues

---
+++ psallakci/.vscode/MyProgram.java/notes.java/runestone/callingmethodsthatreturnvalues.java
@@ -0,0 +1,17 @@
+public class callingmethodsthatreturnvalues {
+   public static void main(String[] args) {
+
+      // void method: it does not return anything.
+      // get methods: return the value of instance variables.
+      // set methods: modifies the value
+      // data type of the variable must match the data type of the return value.
+      // We have to do something(save it to a variable, print it) with the values that
+      // the *get methods* return.
+      // toString() method: returns a value.
+      // it tries to convert an object to a String.
+      // return statement
+      // getDistance(); finds the distance from a given point.
+      // to use the return value: it must be stored in a variable or used in a part of
+      // expression.
+   }
+}
