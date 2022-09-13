3021e66cb7b4e4ff31069fc9a99e643beddc16aa
pelinsallakci <112391123+pelinsallakci@users.noreply.github.com>
2022-09-14 01:48:11 +0300

    problem2

---
+++ psallakci/.vscode/MyProgram.java/euler/evenfibonaccinumbers.java
@@ -0,0 +1,19 @@
+public class evenfibonaccinumbers {
+    public static void main(String[] args) {
+
+        int firstNumber = 1;
+        int secondNumber = 2;
+        int thirdNumber;
+        System.out.print(firstNumber + " " + secondNumber);
+        for (int i=1; i<100; i++) {
+            if (i%2 = 0); {
+           thirdNumber = firstNumber + secondNumber;
+            System.out.print(thirdNumber + " ");
+            firstNumber = secondNumber;
+            secondNumber = thirdNumber;
+            }
+        }
+        
+
+    }
+}
