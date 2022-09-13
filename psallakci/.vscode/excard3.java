17f485004db014d1250798d33a5d19e49f4cf169
pelinsallakci <112391123+pelinsallakci@users.noreply.github.com>
2022-09-13 15:18:17 +0300

    excard3

---
+++ psallakci/.vscode/excard3.java
@@ -0,0 +1,20 @@
+public class excard3 {
+    public static void main(String[] args) {
+
+        int number1; // it gives error if you don't use it at the rest of the code, otherwise it
+                     // doesn't.
+        System.out.println(number1); // once it is printed it gives a error
+
+        String number2; // it gives error if you don't use it at the rest of the code, otherwise it
+                        // doesn't.
+        System.out.println(number2); // once it is printed it gives a error
+        // it gives an error since number1 and number2 is not initialized.
+        // the error could be resolved by initializing a value to the variables number1
+        // and number2.
+
+        String number4 = new String();
+        System.out.println(number4);
+
+        //
+    }
+}
