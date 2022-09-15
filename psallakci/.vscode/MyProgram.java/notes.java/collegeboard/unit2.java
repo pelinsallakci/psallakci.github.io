152826a44cd5e146e9f19cff6ae39a3ecf81c5f8
pelinsallakci <112391123+pelinsallakci@users.noreply.github.com>
2022-09-15 20:05:31 +0300

    unit2

---
+++ psallakci/.vscode/MyProgram.java/notes.java/collegeboard/unit2.java
@@ -0,0 +1,36 @@
+public class unit2 {
+    public static void main(String[] args) {
+        // Topic 2.1 video1
+        // class is a blueprint(ex)for creating objects with same behavior different
+        // attributes.
+        // object: specific entity, made from class (it can be manipulated).
+        // objects are instances of classes with variables used to name the objects.
+        // objects has behaviors and attributes.
+
+        // Topic 2.1 video2
+        // exercise yaptim
+
+        // Topic 2.2 video1
+        // Constructors: initializes the attributes for an object.
+        // constructors starts with "public" and has the same name as the class
+        // the signature: contains the name of the constructor and the parameter list.
+        // parameter list: used to initialize the values.
+        // formal parameters: variable that receives the value inside the constructor
+        // actual parameters(asrgument): the actual value that is passed to the
+        // constructor.
+        // call by value: passing values to constructor
+        // overloading constructor: having more than one constructor for an object.
+        // no argument constructor: has no parameters, sets the instance variables for
+        // the object to default values.
+
+        // Topic 2.2 video2
+        // exercise yaptim
+
+        // Topic 2.2 video3
+        // variables are defined the correct types to represent reference data.
+        // Null is used to create a reference to an object that doesn't contain
+        // anything.
+        // empty reference could be set equal to a new object.
+    }
+
+}
