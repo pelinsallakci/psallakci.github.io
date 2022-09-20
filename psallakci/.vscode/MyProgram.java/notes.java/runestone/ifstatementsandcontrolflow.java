7e2d5074c4f8d20a9faef89d7a9a4f2c09524738
pelinsallakci <112391123+pelinsallakci@users.noreply.github.com>
2022-09-20 22:55:03 +0300

    ifstatements

---
+++ psallakci/.vscode/MyProgram.java/notes.java/runestone/ifstatementsandcontrolflow.java
@@ -0,0 +1,10 @@
+public class ifstatementsandcontrolflow {
+    public static void main(String[] args) {
+
+        // if (boolean expression){
+        // code to run
+        // }
+        // we should use == in if statements,
+        // since we expect a boolean output(true, false).
+    }
+}
