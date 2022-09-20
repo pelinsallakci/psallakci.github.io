ed700ed06f2b49df6449f7cbcacffcc2fbe6b619
pelinsallakci <112391123+pelinsallakci@users.noreply.github.com>
2022-09-20 22:30:38 +0300

    booleanexpressions

---
+++ psallakci/.vscode/MyProgram.java/notes.java/runestone/booleanexpressions.java
@@ -0,0 +1,18 @@
+public class booleanexpressions {
+    public static void main(String[] args) {
+        // == and != returns true or false values
+        // usage of mod operator (%):
+        // (x%2==0) even number
+        // (x%2>0) odd number
+        // (x%n == 0) if x is a multiple of n.
+        // (x%10==lastDigit) used to find the last digit of a number x
+        // (num%60) used to get the numbe rof minutes left when converted to hours
+        // *** tekrar bak(num%limit) used if you have limit in the value, and you need
+        // to wrap around to the front if the value goes over the limit
+        //
+        //
+        //
+        //
+        //
+    }
+}
